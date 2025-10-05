1535813b12480c6d8d6b2931041682faa5ffc7a7
fork_me_maybe <forkmemaybe@gmail.com>
2025-10-05 23:09:50 +0530

    comment password validator

--- help_desk/settings/common.py
+++ help_desk/settings/common.py
@@ -82,9 +82,9 @@ WSGI_APPLICATION = "help_desk.wsgi.application"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
-    {
-        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
-    },
+    # {
+    #     "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
+    # },
     {
         "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
     },
